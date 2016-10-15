8ea630e745e9e55b968d1bc049666a2703533081
toni <unmail.toni@gmail.com>
2016-10-15 12:33:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Select from './Select'
+
+const rmc ={
+  Select
+}
+
+export { Select }
+
+export default rmc
+//'use strict';
+//var Select = require('./Select');
+//module.exports.Select = Select;
+//module.exports.default={
+//  Select: Select
+//};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+    //publicPath: '/dist',
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname+'/dist',
+    library:'react-minimal-components',
+    libraryTarget:'umd',
+    filename: 'react-minimial-components.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
