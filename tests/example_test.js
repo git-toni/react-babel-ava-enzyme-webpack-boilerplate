0c79ecef8543d0fca5f6cca1830cd50be992f48a
toni <unmail.toni@gmail.com>
2016-11-03 22:35:46 +0100

    Fixing tests

--- tests/example_test.js
+++ tests/example_test.js
@@ -1,12 +1,10 @@
 import test from 'ava';
 import React from 'react';
 import {shallow} from 'enzyme';
-import {Select} from '../src/index'
-import {isObject,finder} from '../src/utils'
 
 
 
 test('renders a select tag with no options', t => {
   const wrapper = shallow(<div />);
-  t.is(wrapper.find('dv').length, 1);
+  t.is(wrapper.find('div').length, 1);
 });
